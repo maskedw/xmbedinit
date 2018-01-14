c60013775ec25e79ca982ad5db4d86f000a87de5
MaskedW <maskedw00@gmail.com>
2018-01-14 15:57:52 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='xmbedinit',
+    version='0.1.0',
+    description='This tool will help local development of mbed',
+    long_description='',
+    author='MaskedW',
+    author_email='maskedw@gmail.com',
+    url='',
+    license='MIT License',
+    packages=find_packages(exclude=('tests', 'docs')),
+    package_data={'xmbedinit': ['templates/*']},
+    entry_points={
+        'console_scripts': ['xmbedinit= xmbedinit.xmbedinit:main'],
+    },
+    zip_safe=False,
+    install_requires=['']
+)
---
+++ xmbedinit/xmbedinit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import with_statement
+
+
+def main():
+    print('Hello xmbedinit')
+
+
+if __name__ == "__main__":
+    main()
